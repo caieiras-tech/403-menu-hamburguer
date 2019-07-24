6fa6a06d09729f18fa3eae64d626df6eac949677
Lucas Melo <luucasmell@gmail.com>
2019-07-24 16:32:07 -0300

    Adicionar animacao do  menu

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let hamburguer = document.querySelector("#hamburguer");
 let menu = document.querySelector("section");
+let tracos = document.querySelectorAll("span");
+
 
 function controlarMenu(){
     menu.classList.toggle("aparecerMenu");
+    tracos[1].classList.toggle("sumirIgualTiro");
+    tracos[0].classList.toggle("virarX1");
+    tracos[2].classList.toggle("virarX2");
 }
 
 hamburguer.onclick = controlarMenu;
